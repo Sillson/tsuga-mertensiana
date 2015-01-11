10925ad3b215607a4df5d219c8eb0e10cf02f24f
Stuart Illson <sillson88@gmail.com>
2015-01-11 12:01:28 -0800

    listing all GC projects

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,7 @@ class HomeController < ApplicationController
 
   def index
     @api = GatherContentApi.new(ENV["ORGANIZATION"], ENV["API_KEY"], 'x')
+    @mash = Hashie::Mash.new(@api.get_projects)
+    @mash.shift
   end
 end

0396b9c32c0f0ae5d7477e4981395512c1690080
Stuart Illson <sillson88@gmail.com>
2015-01-11 22:31:19 -0800

    get pages by project. nesting en view is going to suck

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,8 @@ class HomeController < ApplicationController
   require 'gc_fetch/gc_fetch'
 
   def index
-    @api = GatherContentApi.new(ENV["ORGANIZATION"], ENV["API_KEY"], 'x')
-    mash = Hashie::Mash.new(@api.get_projects)
+    @@api = GatherContentApi.new(ENV["ORGANIZATION"], ENV["API_KEY"], 'x')
+    mash = Hashie::Mash.new(@@api.get_projects)
     mash.shift #Can append .shift to api call in console, won't run on server.
     clean_mash = mash.projects.each {|clean| clean_ampersands(clean.name)}
     @user_projects = clean_mash.sort_by {|project| project.name}
@@ -11,5 +11,8 @@ class HomeController < ApplicationController
 
   def project_overview
     @magic_number = params[:id]
+    @magic_name = params[:name]
+    mash = Hashie::Mash.new(@@api.get_pages_by_project(@magic_number))
+    mash.shift
   end
 end
